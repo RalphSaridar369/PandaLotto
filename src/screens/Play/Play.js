ec23686f146a0c56e92df6f0645cfb421cd2011e
Nadimfr <nadim.farah@hotmail.com>
2022-04-15 14:08:21 +0300

    Play Stepper

---
+++ src/screens/Play/Play.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Play.scss";
+
+import Stepper from "../../components/Stepper/Stepper";
+
+const myStyle = {
+  backgroundColor: "#090021",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
+
+const Play = () => {
+  return (
+    <div className="main-play" style={myStyle}>
+      <div className="containers">
+        <div className="title">
+          <div className="title-part1">
+            <h2>Buy your ticket</h2>
+            <div className="separator"></div>
+            <h3>March 1, 2022</h3>
+          </div>
+          <div className="title-part2">
+            <h3>Next Draw in:</h3>
+            <h2>53:45</h2>
+          </div>
+        </div>
+        <div className="line"></div>
+
+        <Stepper />
+
+        <div className="Buttons">
+          <div className="Button">
+            <h2>Select</h2>
+          </div>
+          <div className="Button">
+            <h2>Select</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Play;
--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -1,34 +1,41 @@
-import Home from '../screens/Home/Home'
-import Login from '../screens/Login/Login'
-import ProtectedPath from '../screens/ProtectedPath/ProtectedPath'
-import Register from '../screens/Register/Register'
+import Home from "../screens/Home/Home";
+import Login from "../screens/Login/Login";
+import ProtectedPath from "../screens/ProtectedPath/ProtectedPath";
+import Register from "../screens/Register/Register";
+import Play from "../screens/Play/Play";
 
 export const normalRoutes = [
-    {   
-        exact:true,
-        path:"/",
-        component:<Home/>,
-        header:true
-    },
-    {   
-        exact:false,
-        path:"/login",
-        component:<Login/>,
-        header:false
-    },
-    {   
-        exact:false,
-        path:"/register",
-        component:<Register/>,
-        header:false
-    },
-]
+  {
+    exact: true,
+    path: "/",
+    component: <Home />,
+    header: true,
+  },
+  {
+    exact: false,
+    path: "/login",
+    component: <Login />,
+    header: false,
+  },
+  {
+    exact: false,
+    path: "/register",
+    component: <Register />,
+    header: false,
+  },
+  {
+    exact: false,
+    path: "/play",
+    component: <Play />,
+    header: false,
+  },
+];
 
 export const loggedInRoutes = [
-    {   
-        exact:false,
-        path:"/protected",
-        component:<ProtectedPath/>,
-        header:true
-    },
-]
+  {
+    exact: false,
+    path: "/protected",
+    component: <ProtectedPath />,
+    header: true,
+  },
+];
