cc56abec636bba91eda241708ee0ac306d547f01
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-04-25 14:12:27 +0300

    Added select component

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ChangePassword from './Screens/ChangePassword';
 import MyTickets from './Screens/MyTickets';
 import MyWallet from './Screens/MyWallet';
 import './Settings.scss';
 
 const Settings = () => {
+  const navigate = useNavigate();
   const [screenState, setScreenState] = useState(1);
   const [changePasswordData, setChangePasswordData] = useState({
     email: '',
@@ -20,6 +22,12 @@ const Settings = () => {
     alert("Password changed successfully")
   }
 
+  let options = [
+    {id:1, value:'Paypal'},
+    {id:2, value:'Bitcoin'},
+    {id:3, value:'Wallet'},
+  ]
+
   let screens = [
     { id: 1, value: 'Edit Account' },
     { id: 2, value: 'Wallet' },
@@ -32,7 +40,7 @@ const Settings = () => {
       case 1:
         return <ChangePassword setChangePassword={setChangePassword} handleSubmitPassword={handleSubmitPassword} />
       case 2:
-        return <MyWallet />
+        return <MyWallet options={options}/>
       case 3:
         return <MyTickets />
     }
@@ -51,7 +59,7 @@ const Settings = () => {
               if(item.id!=4){
                 setScreenState(item.id);
               }
-              else{}
+              else navigate('/login')
             }}>
               <div className='settings-option-container'>
                 <div className={`settings-option ${item.id === screenState && "active-setting"}`}
