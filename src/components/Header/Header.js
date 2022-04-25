e29488b7263aca907494912ef684ba99bebc31d7
Nadimfr <nadim.farah@hotmail.com>
2022-04-25 17:04:12 +0300

    Menu responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { clear, getItem, removeItem, setItem } from "../../utils/LocalStorage";
 import { MainContext } from "../../utils/MainContext";
 import Logo from "../../images/PandaLotto.png";
@@ -6,8 +6,10 @@ import AccountLogo from "../../icons/Account.svg";
 import ArrowDown from "../../icons/down.svg";
 import Money from "../../icons/Money.svg";
 import "./Header.scss";
+import { display, style } from "@mui/system";
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const { loggedIn, setLogIn } = useContext(MainContext);
   const handleLogin = () => {
     if (loggedIn) {
@@ -19,61 +21,122 @@ const Header = () => {
   };
   return (
     <div className="main__header">
-      <div className="nav">
-        <img className="logo" src={Logo} />
-        <div className="burger">|||</div>
-      </div>
-      <div className="right">
-        <p>Previous Results</p>
-        <p onClick={handleLogin}>
-          {loggedIn ? (
-            <div className="account">
-              <img src={AccountLogo} />
-              <p>Hello, UserID</p>
-              <img src={ArrowDown} />
-
-              <div className="money">
-                <img src={Money} />
-                <p>
-                  <span>$</span>500.00
-                </p>
-              </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="nav">
+          {isMobile === false ? <img className="logo" src={Logo} /> : null}
+          {isMobile === false ? (
+            <div className="burger" onClick={() => setIsMobile(!isMobile)}>
+              |||
             </div>
-          ) : (
-            "Login/Register"
-          )}
-        </p>
-        <div className="special">
-          <p>Let's Play</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25.062"
-            height="18.477"
-            viewBox="0 0 25.062 18.477"
-          >
-            <g
-              id="Group_1"
-              data-name="Group 1"
-              transform="translate(-402.75 -177.182)"
+          ) : null}
+        </div>
+        <div className="right">
+          <p>Previous Results</p>
+          <p onClick={handleLogin}>
+            {loggedIn ? (
+              <div className="account">
+                <img src={AccountLogo} />
+                <p>Hello, UserID</p>
+                <img src={ArrowDown} />
+
+                <div className="money">
+                  <img src={Money} />
+                  <p>
+                    <span>$</span>500.00
+                  </p>
+                </div>
+              </div>
+            ) : (
+              "Login/Register"
+            )}
+          </p>
+          <div className="special">
+            <p>Let's Play</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25.062"
+              height="18.477"
+              viewBox="0 0 25.062 18.477"
             >
-              <path
-                id="Path_3"
-                data-name="Path 3"
-                d="M414.7,195.66a1,1,0,0,1-.718-.3,1.022,1.022,0,0,1,0-1.436l7.506-7.506-7.506-7.506a1.016,1.016,0,0,1,1.436-1.436l8.224,8.224a1.022,1.022,0,0,1,0,1.436l-8.224,8.224A1,1,0,0,1,414.7,195.66Z"
-                transform="translate(3.88)"
-                fill="#fff"
-              />
-              <path
-                id="Path_4"
-                data-name="Path 4"
-                d="M426.569,185.282h-22.8a1.016,1.016,0,1,1,0-2.032h22.8a1.016,1.016,0,1,1,0,2.032Z"
-                transform="translate(0 2.153)"
-                fill="#fff"
-              />
-            </g>
-          </svg>
+              <g
+                id="Group_1"
+                data-name="Group 1"
+                transform="translate(-402.75 -177.182)"
+              >
+                <path
+                  id="Path_3"
+                  data-name="Path 3"
+                  d="M414.7,195.66a1,1,0,0,1-.718-.3,1.022,1.022,0,0,1,0-1.436l7.506-7.506-7.506-7.506a1.016,1.016,0,0,1,1.436-1.436l8.224,8.224a1.022,1.022,0,0,1,0,1.436l-8.224,8.224A1,1,0,0,1,414.7,195.66Z"
+                  transform="translate(3.88)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_4"
+                  data-name="Path 4"
+                  d="M426.569,185.282h-22.8a1.016,1.016,0,1,1,0-2.032h22.8a1.016,1.016,0,1,1,0,2.032Z"
+                  transform="translate(0 2.153)"
+                  fill="#fff"
+                />
+              </g>
+            </svg>
+          </div>
         </div>
       </div>
+
+      {isMobile === true ? (
+        <div className="main-mobile">
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div className="nav">
+              {isMobile === true ? (
+                <div className="close" onClick={() => setIsMobile(!isMobile)}>
+                  X
+                </div>
+              ) : null}
+            </div>
+
+            <div className="body">
+              <h1>
+                Download<br></br>Our mobile app
+              </h1>
+
+              <div className="download">
+                <div className="download-app">App Store</div>
+                <div className="download-app">Play Store</div>
+              </div>
+
+              <div className="menu">
+                <p>Previous Results</p>
+                <p>Let’s Play</p>
+                <p>Why Panda Lotto</p>
+                <p>Contact</p>
+                <p>Jackpot</p>
+                <p>Lucky Winners</p>
+              </div>
+
+              <div className="footer">
+                <p>Terms & Conditions</p>
+                <div className="bar"></div>
+                <p>Privacy Policy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Checkbox } from '../../components/Checkbox/Checkbox';
-import { TextInput } from '../../components/TextInput/Textinput';
-import './Login.scss';
-import Background from '../../images/background.png';
-import Dots from '../../images/dots.png';
-import { formValidator } from '../../utils/FormValidator';
-import { setItem } from '../../utils/LocalStorage';
-import Cookies from 'universal-cookie';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Checkbox } from "../../components/Checkbox/Checkbox";
+import { TextInput } from "../../components/TextInput/Textinput";
+import "./Login.scss";
+import Background from "../../images/background.png";
+import Dots from "../../images/dots.png";
+import { formValidator } from "../../utils/FormValidator";
+import { setItem } from "../../utils/LocalStorage";
+import Cookies from "universal-cookie";
 
 const Login = () => {
   const cookie = new Cookies();
   const navigate = useNavigate();
   const [data, setData] = useState({
-    userId: '',
-    password: '',
+    userId: "",
+    password: "",
     remember_me: false,
-  })
+  });
 
   useEffect(() => {
-    console.log('effect cookie: ', cookie.get('user'))
-  }, [])
+    console.log("effect cookie: ", cookie.get("user"));
+  }, []);
 
   const myStyle = {
-    backgroundColor: '#090021',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
+    backgroundColor: "#090021",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
   };
 
   // const dotsStyle = {
@@ -37,100 +37,118 @@ const Login = () => {
   // }
 
   const handleCookie = (e) => {
-    console.log(e)
+    console.log(e);
     let { userId, password } = data;
     if (userId.length < 1 || password.length < 1) {
-      alert("Make sure you fill both id and password first")
+      alert("Make sure you fill both id and password first");
+    } else if (!e.target.checked) {
+      setData({ ...data, remember_me: e.target.checked });
+      console.log(1);
+      cookie.set(
+        "user",
+        { userId, password },
+        {
+          path: "/",
+        }
+      );
+    } else {
+      console.log("cookie: ", cookie.get("user"));
+      setData({ ...data, remember_me: e.target.checked });
+      cookie.remove("user");
+      console.log(2);
     }
-    else if (!e.target.checked) {
-      setData({ ...data, remember_me: e.target.checked })
-      console.log(1)
-      cookie.set("user", { userId, password }, {
-        path: "/"
-      });
-    }
-    else {
-      console.log('cookie: ', cookie.get('user'))
-      setData({ ...data, remember_me: e.target.checked })
-      cookie.remove('user');
-      console.log(2)
-    }
-  }
+  };
 
   const handleLogin = () => {
-    let { userId, password } = data
-    formValidator({ userId, password }, 'login', () => {
+    let { userId, password } = data;
+    formValidator({ userId, password }, "login", () => {
       setItem("user-data", { userId, password });
       setItem("token", 1231);
       navigate("/");
-    })
-  }
+    });
+  };
 
   return (
-      <div className='main-login' style={myStyle}>
-        <div className='containers'>
-          <div className='sign-in-container'>
-            <h2 className='sign-in-header'>
-              Sign in
-            </h2>
-            <div className='inputs-container'>
-              <div className='input'>
-                <div className='input-label'>
-                  User ID*
-                </div>
-                <TextInput value={data.userId} onChange={(e) => setData({ ...data, userId: e.target.value })} />
-              </div>
-              <div className='input'>
-                <div className='input-label'>
-                  Password*
-                </div>
-                <TextInput value={data.password} hideText onChange={(e) => setData({ ...data, password: e.target.value })} />
-              </div>
+    <div className="main-login" style={myStyle}>
+      <div className="containers">
+        <div className="sign-in-container">
+          <h2 className="sign-in-header">Sign in</h2>
+          <div className="inputs-container">
+            <div className="input">
+              <div className="input-label">User ID*</div>
+              <TextInput
+                value={data.userId}
+                onChange={(e) => setData({ ...data, userId: e.target.value })}
+              />
             </div>
-            <div className='forgot-password-container'>
-              <div>
-                <Checkbox right={<div className='checkbox-text'>Remember me</div>} value={data.remember_me} onChange={(e) => handleCookie(e)} />
-              </div>
-              <Link to="/" className="forgot-password-link">
-                Forgot your password ?
-              </Link>
-            </div>
-            <div className='signin-button' onClick={handleLogin}>
-              <div className='signin-button-text'>
-                Sign in
-              </div>
-            </div>
-            <div className='login-social-seperator'>
-              <div className='login-social-seperator-line'></div>
-              Or
-              <div className='login-social-seperator-line'></div>
-            </div>
-      
-            <div className='social-media-container'>
-              <div className='social-button' style={{ backgroundColor: '#505eba' }} onClick={() => cookie.get('user')}>
-                <div className='social-button-text'>
-                  <img src={require("../../icons/facebook.png")} className="social-button-icon" />
-                  Login with Facebook
-                </div>
-              </div>
-              <div className='social-media-separator'>
-              </div>
-              <div className='social-button' style={{ backgroundColor: '#5fbcdd' }}>
-                <div className='social-button-text'>
-                  <img src={require("../../icons/twitter.png")} className="social-button-icon" />
-                  Login with Twitter
-                </div>
-              </div>
+            <div className="input">
+              <div className="input-label">Password*</div>
+              <TextInput
+                value={data.password}
+                hideText
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+              />
             </div>
           </div>
-          <div className='create-button'>
-            <Link to="/register" className="create-button-text-1">
-              Don't have an account yet ? <div className="create-button-text-2">Create one for free!</div>
+          <div className="forgot-password-container">
+            <div>
+              <Checkbox
+                right={<div className="checkbox-text">Remember me</div>}
+                value={data.remember_me}
+                onChange={(e) => handleCookie(e)}
+              />
+            </div>
+            <Link to="/" className="forgot-password-link">
+              Forgot your password ?
             </Link>
           </div>
+          <div className="signin-button" onClick={handleLogin}>
+            <div className="signin-button-text">Sign in</div>
+          </div>
+          <div className="login-social-seperator">
+            <div className="login-social-seperator-line"></div>
+            Or
+            <div className="login-social-seperator-line"></div>
+          </div>
+
+          <div className="social-media-container">
+            <div
+              className="social-button"
+              style={{ backgroundColor: "#505eba" }}
+              onClick={() => cookie.get("user")}
+            >
+              <div className="social-button-text">
+                <img
+                  src={require("../../icons/facebook.png")}
+                  className="social-button-icon"
+                />
+                Login with Facebook
+              </div>
+            </div>
+            <div className="social-media-separator"></div>
+            <div
+              className="social-button"
+              style={{ backgroundColor: "#5fbcdd" }}
+            >
+              <div className="social-button-text">
+                <img
+                  src={require("../../icons/twitter.png")}
+                  className="social-button-icon"
+                />
+                Login with Twitter
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="create-button">
+          <Link to="/register" className="create-button-text-1">
+            Don't have an account yet ?{" "}
+            <div className="create-button-text-2">Create one for free!</div>
+          </Link>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
